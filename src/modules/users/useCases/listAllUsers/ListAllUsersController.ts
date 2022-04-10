1918675c551fd34f73115caf6e229789beba2265
Eric Esteves Martins <estevesmartinseric@gmail.com>
2022-04-10 18:07:03 -0300

    finished

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,20 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.headers;
+
+      if (typeof user_id !== "string") {
+        throw new Error("User id is not a string");
+      }
+
+      const users = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.status(200).json(users);
+    } catch (err) {
+      return response.status(400).json({
+        error: err.message});
+    }
   }
 }
 
